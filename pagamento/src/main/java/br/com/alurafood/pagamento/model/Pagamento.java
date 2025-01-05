88196bc7aad42ec127137adf18cd424281308c12
guverissimo <gustavo.verissimo1@outlook.com>
2025-01-05 19:04:01 -0300

    pagamento: add repository, dto

--- pagamento/src/main/java/br/com/alurafood/pagamento/model/Pagamento.java
+++ pagamento/src/main/java/br/com/alurafood/pagamento/model/Pagamento.java
@@ -1,7 +1,10 @@
 package br.com.alurafood.pagamento.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,14 +20,37 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class Pagamento {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private BigDecimal valor;
-  private String nome;
-  private String numero;
-  private String expiracao;
-  private String codigo;
-  private Status status;
-  private Long pedidoId;
-  private Long formaDePagamentoId;
 
+  @NotNull
+  @Positive
+  private BigDecimal valor;
+
+  @NotBlank
+  @Size(max = 90)
+  private String nome;
+
+  @NotBlank
+  @Size(max = 19)
+  private String numero;
+
+  @NotBlank
+  @Size(max = 7)
+  private String expiracao;
+
+  @NotBlank
+  @Size(max = 3, min = 3)
+  private String codigo;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private Status status;
+
+  @NotNull
+  private Long pedidoId;
+
+  @NotNull
+  private Long formaDePagamentoId;
 }
